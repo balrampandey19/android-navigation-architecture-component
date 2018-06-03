73611f26d1ef3e2b7109deb25f9e24786b380d91
Balram Pandey <balrampandey19@gmail.com>
2018-06-03 15:06:50 +0530

    Fragment fixes

--- app/src/main/java/troops/com/android_navigation/OnBoardingFragment.kt
+++ app/src/main/java/troops/com/android_navigation/OnBoardingFragment.kt
@@ -1,7 +1,4 @@
 package troops.com.android_navigation
-
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
