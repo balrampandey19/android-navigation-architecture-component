291fe337c28368ef98f075716f28d6c7d9c51995
Balram Pandey <balrampandey19@gmail.com>
2018-06-04 00:41:44 +0530

    Fixes

--- app/src/main/java/troops/com/android_navigation/OnBoardingFragment.kt
+++ app/src/main/java/troops/com/android_navigation/OnBoardingFragment.kt
@@ -1,14 +1,24 @@
 package troops.com.android_navigation
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 
 class OnBoardingFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.signIn)?.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.signInFragment, null))
     }
 }
