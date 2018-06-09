1a758d3a6d05a6352ee4e6c097649382b71599fa
Balram Pandey <balrampandey19@gmail.com>
2018-06-09 23:38:08 +0530

    Home Sscreen Design

--- app/src/main/java/troops/com/android_navigation/SignInFragment.kt
+++ app/src/main/java/troops/com/android_navigation/SignInFragment.kt
@@ -4,17 +4,33 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.*
 
 
 class SignInFragment : Fragment() {
+
+    private var nameEditText: EditText? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tv = view.findViewById<TextView>(R.id.email)
+        val bundle = Bundle()
+        bundle.putString("data", tv.text.toString())
+
     }
 
 }
