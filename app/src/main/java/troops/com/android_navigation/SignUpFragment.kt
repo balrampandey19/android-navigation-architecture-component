c576f95c53e7a9e66f0f640cc308269b282ce8dc
Balram Pandey <balrampandey19@gmail.com>
2018-06-11 23:54:52 +0530

    action navigation fix

--- app/src/main/java/troops/com/android_navigation/SignUpFragment.kt
+++ app/src/main/java/troops/com/android_navigation/SignUpFragment.kt
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
 
 
 class SignUpFragment : Fragment() {
@@ -15,5 +18,11 @@ class SignUpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.signUp)?.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.next_action, null)
+        )
 
+    }
 }
